eb41ab5b4ef83f4b8073f400870dbed857828656
Sarah Sanders <sarah.nicholson.13@ucl.ac.uk>
2022-10-17 09:41:09 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+setup(
+  name='comp0035-data-prep',
+  version='1.0',
+  author='Sarah Sanders',
+  url='https://github.com/nicholsons/comp0035-data-prep',
+  python_requires='>=3.7',
+  packages=find_packages(
+    include=[
+    ]
+  ),
+  install_requires=[
+        'pandas,
+        'numpy,
+        'matplotlib,
+    ],
+    package_data={
+        'cgd': ['cgd_raw.csv'],
+    },
+)
